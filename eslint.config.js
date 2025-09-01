7992efb0f36ed1d747e0f673e44098b14250b126
GabrielJaccoud <107004489+GabrielJaccoud@users.noreply.github.com>
2025-09-01 10:30:20 -0300

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { files: ['**/*.{js,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
+];
